1c29f2d1c6e12c03d438fb60af0cc43beda4734a
skland12 <109992780+skland12@users.noreply.github.com>
2025-05-22 10:56:25 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function exempleVar() {
-var x = 10; //Déclaration
-if (true) {
-    let x = 20 //Réécriture de la même variable
-    console.log(x) ; //Affiche : 20
-}
-console.log(x) ; // Affiche : 20 (pas de portée bloc)
-}
-exempleVar(); 
